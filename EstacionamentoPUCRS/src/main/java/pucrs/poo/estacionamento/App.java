4395fa7e9de199853aa8d6827ffb756c023fbf9a
Bruno Pedralli <brunopedralli67@gmail.com>
2025-11-17 15:23:18 +0000

    Leitura de Arquivos fix

--- EstacionamentoPUCRS/src/main/java/pucrs/poo/estacionamento/App.java
+++ EstacionamentoPUCRS/src/main/java/pucrs/poo/estacionamento/App.java
@@ -1,13 +1,11 @@
 package pucrs.poo.estacionamento;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        CadastroClientes cadCli = CadastroClientes.getInstance();
-
-        
-        //SpringApplication.run(App.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
