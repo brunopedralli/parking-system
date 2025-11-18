3298fa24604b7c359374a635c71b3596e0cdd92b
Bruno Pedralli <brunopedralli67@gmail.com>
2025-11-18 23:15:54 +0000

    Delete entrada & saiad

--- EstacionamentoPUCRS/src/main/java/pucrs/poo/estacionamento/vaadin/Estacionamento.java
+++ EstacionamentoPUCRS/src/main/java/pucrs/poo/estacionamento/vaadin/Estacionamento.java
@@ -6,16 +6,12 @@ import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouterLink;
 
 @PageTitle("Estacionamento")
 @Route("estacionamento")
 public class Estacionamento extends VerticalLayout {
     public Estacionamento() {
         add(new Hr());
-
-        add(new RouterLink("Entrada de veículos", Entrada.class));
-        add(new RouterLink("Saída de veículos", Saida.class));
 
         Button backButton = new Button("Voltar");
         backButton.addClickListener(e -> UI.getCurrent().navigate(""));
